96a13a0531cf0587288ded8596381af9817537e1
Ruslan Bogdanov <russian.bogd@gmail.com>
2022-04-27 02:27:34 +0300

    feat: add file

---
+++ src/queue.js
@@ -0,0 +1,47 @@
+const { NotImplementedError } = require('../extensions/index.js');
+
+const { ListNode } = require('../extensions/list-node.js');
+
+/**
+ * Implement the Queue with a given interface via linked list (use ListNode extension above).
+ *
+ * @example
+ * const queue = new Queue();
+ *
+ * queue.enqueue(1); // adds the element to the queue
+ * queue.enqueue(3); // adds the element to the queue
+ * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
+ * queue.getUnderlyingList() // returns { value: 3, next: null }
+ */
+class Queue {
+  constructor() {
+    this.list = null;
+    this.queue = null;
+  }
+
+  getUnderlyingList() {
+    return this.list;
+  }
+
+  enqueue(value) {
+    const node = new ListNode(value);
+    if (!this.list) {
+      this.list = this.queue = node;
+    } else {
+      this.queue.next = node;
+      this.queue = node;
+    }
+
+  }
+
+  dequeue() {
+    const val = this.list.value;
+    this.list = this.list.next;
+
+    return val
+  }
+}
+
+module.exports = {
+  Queue
+};
---
+++ src/remove-from-list.js
@@ -0,0 +1,49 @@
+const { NotImplementedError } = require('../extensions/index.js');
+
+const { ListNode } = require('../extensions/list-node.js');
+
+/**
+ * Given a singly linked list of integers l and an integer k,
+ * remove all elements from list l that have a value equal to k.
+ *
+ * @param {List} l
+ * @param {Number} k
+ * @return {List}
+ *
+ * @example
+ * For l = [3, 1, 2, 3, 4, 5] and k = 3,
+ * the output should be [1, 2, 4, 5]
+ *
+ * Singly - linked lists are already defined using interface
+ * class ListNode {
+ *   constructor(x) {
+ *     this.value = x;
+ *     this.next = null;
+ *   }
+ * }
+ */
+function removeKFromList(l, k) {
+  let deleten = null;
+
+  while (l && l.value === k) {
+    deleten = l.value;
+    l = l.next;
+  }
+
+  let currentNode = l;
+
+  while (currentNode.next) {
+    if (currentNode.next.value === k) {
+      deleten = currentNode.next;
+      currentNode.next = currentNode.next.next;
+    } else {
+      currentNode = currentNode.next;
+    }
+  }
+
+  return l;
+}
+
+module.exports = {
+  removeKFromList
+};
